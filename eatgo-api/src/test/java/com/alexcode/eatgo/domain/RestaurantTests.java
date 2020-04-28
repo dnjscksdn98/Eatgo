e8a61ff39280a678b9e59d7eee683540dad81067
Alex <dnjscksdn98@gmail.com>
2020-04-28 22:53:11 +0900

    Test: 테스트 실행

---
+++ eatgo-api/src/test/java/com/alexcode/eatgo/domain/RestaurantTests.java
@@ -0,0 +1,23 @@
+package com.alexcode.eatgo.domain;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.core.Is.is;
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class RestaurantTests {
+
+  @Test
+  public void creation() {
+    Restaurant restaurant = new Restaurant("Bob zip", "Seoul");
+    assertThat(restaurant.getName(), is("Bob zip"));
+    assertThat(restaurant.getAddress(), is("Seoul"));
+  }
+
+  @Test
+  public void information() {
+    Restaurant restaurant = new Restaurant("Bob zip", "Seoul");
+    assertThat(restaurant.getInformation(), is("Bob zip in Seoul"));
+  }
+}
