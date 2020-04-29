912ac6bac51bc9be8ade00a555e9884ff73a0623
Alex <dnjscksdn98@gmail.com>
2020-04-29 22:55:19 +0900

    Test: 가게 추가 POST API test

--- eatgo-api/src/test/java/com/alexcode/eatgo/interfaces/RestaurantControllerTest.java
+++ eatgo-api/src/test/java/com/alexcode/eatgo/interfaces/RestaurantControllerTest.java
@@ -1,9 +1,13 @@
 package com.alexcode.eatgo.interfaces;
 
 import static org.hamcrest.core.StringContains.containsString;
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.verify;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.header;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import com.alexcode.eatgo.application.RestaurantService;
@@ -16,6 +20,7 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
 
@@ -52,5 +57,17 @@ class RestaurantControllerTest {
         .andExpect(content().string(containsString("\"name\":\"Bob zip\"")))
         .andExpect(content().string(containsString("\"id\":1004")))
         .andExpect(content().string(containsString("Kimchi")));
+  }
+
+  @Test
+  public void create() throws Exception {
+    mvc.perform(post("/restaurants")
+        .contentType(MediaType.APPLICATION_JSON)
+        .content("{\"name\": \"BeRyong\", \"address\": \"Seoul\"}"))
+        .andExpect(status().isCreated())
+        .andExpect(header().string("location", "/restaurants/1234"))
+        .andExpect(content().string("{}"));
+
+    verify(restaurantService).addRestaurant(any());
   }
 }
