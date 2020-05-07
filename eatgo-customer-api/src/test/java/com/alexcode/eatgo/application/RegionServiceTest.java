3cf1c449a1ea7ea3c488cbaa3f05ecaaac1ef99c
Alex <dnjscksdn98@gmail.com>
2020-05-07 16:38:41 +0900

    Test: 고객 API의 지역 도메인 모델 테스트

---
+++ eatgo-customer-api/src/test/java/com/alexcode/eatgo/application/RegionServiceTest.java
@@ -0,0 +1,43 @@
+package com.alexcode.eatgo.application;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.core.Is.is;
+import static org.mockito.BDDMockito.given;
+
+import com.alexcode.eatgo.domain.Region;
+import com.alexcode.eatgo.domain.RegionRepository;
+import java.util.ArrayList;
+import java.util.List;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+class RegionServiceTest {
+
+  private RegionService regionService;
+
+  @Mock
+  private RegionRepository regionRepository;
+
+  @BeforeEach
+  public void setUp() {
+    MockitoAnnotations.initMocks(this);
+
+    regionService = new RegionService(regionRepository);
+  }
+
+  @Test
+  public void getRegions() {
+    List<Region> mockRegions = new ArrayList<>();
+    mockRegions.add(Region.builder().name("Seoul").build());
+
+    given(regionRepository.findAll()).willReturn(mockRegions);
+
+    List<Region> regions = regionService.getRegions();
+
+    Region region = regions.get(0);
+    assertThat(region.getName(), is("Seoul"));
+  }
+
+}
