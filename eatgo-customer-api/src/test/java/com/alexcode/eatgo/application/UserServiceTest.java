710831d39833eba46274cbe2457c18e2c9203d1e
Alex <dnjscksdn98@gmail.com>
2020-05-10 19:55:20 +0900

    Test: 사용자 회원가입 테스트

---
+++ eatgo-customer-api/src/test/java/com/alexcode/eatgo/application/UserServiceTest.java
@@ -0,0 +1,58 @@
+package com.alexcode.eatgo.application;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.never;
+import static org.mockito.Mockito.verify;
+
+import com.alexcode.eatgo.domain.User;
+import com.alexcode.eatgo.domain.UserRepository;
+import java.util.Optional;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+class UserServiceTest {
+
+  private UserService userService;
+
+  @Mock
+  private UserRepository userRepository;
+
+  @BeforeEach
+  public void setUp() {
+    MockitoAnnotations.initMocks(this);
+
+    userService = new UserService(userRepository);
+  }
+
+  @Test
+  public void registerUser() {
+    String email = "tester@example.com";
+    String name = "tester";
+    String password = "testerpw";
+
+    userService.registerUser(email, name, password);
+
+    verify(userRepository).save(any());
+  }
+
+  @Test
+  public void registerUserWithExistedEmail() {
+    String email = "tester@example.com";
+    String name = "tester";
+    String password = "testerpw";
+
+    User user = User.builder().build();
+
+    Assertions.assertThrows(EmailExistsException.class, () -> {
+      given(userRepository.findByEmail(email)).willReturn(Optional.of(user));
+
+      userService.registerUser(email, name, password);
+
+      verify(userRepository, never()).save(any());
+    });
+  }
+}
---
+++ eatgo-customer-api/src/test/java/com/alexcode/eatgo/interfaces/UserControllerTest.java
@@ -0,0 +1,55 @@
+package com.alexcode.eatgo.interfaces;
+
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.verify;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.header;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import com.alexcode.eatgo.application.UserService;
+import com.alexcode.eatgo.domain.User;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.web.servlet.MockMvc;
+
+@ExtendWith(SpringExtension.class)
+@WebMvcTest(UserController.class)
+class UserControllerTest {
+
+  @Autowired
+  private MockMvc mvc;
+
+  @MockBean
+  private UserService userService;
+
+  @Test
+  public void create() throws Exception {
+    String email = "tester@example.com";
+    String name = "tester";
+    String password = "testerpw";
+
+    User mockUser = User.builder()
+        .id(1L)
+        .email(email)
+        .name(name)
+        .password(password)
+        .build();
+
+    given(userService.registerUser(email, name, password)).willReturn(mockUser);
+
+    mvc.perform(post("/users")
+        .contentType(MediaType.APPLICATION_JSON)
+        .content("{\"email\": \"tester@example.com\", \"name\": \"tester\", \"password\": \"testerpw\"}"))
+        .andExpect(status().isCreated())
+        .andExpect(header().string("location", "/users/1"));
+
+    verify(userService).registerUser(eq(email), eq(name), eq(password));
+  }
+
+}
