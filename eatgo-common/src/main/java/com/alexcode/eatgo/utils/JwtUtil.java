0b72be1b9e5970993a1113cd3d983b4edf180fa4
Alex <dnjscksdn98@gmail.com>
2020-06-24 10:06:31 +0900

    JWT를 전달받아서 안에 담긴 Claims 객체를 반환해주는 함수 생성

--- eatgo-common/src/main/java/com/alexcode/eatgo/utils/JwtUtil.java
+++ eatgo-common/src/main/java/com/alexcode/eatgo/utils/JwtUtil.java
@@ -1,5 +1,6 @@
 package com.alexcode.eatgo.utils;
 
+import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.security.Keys;
@@ -10,18 +11,48 @@ public class JwtUtil {
 
     private Key key;
 
+    /**
+     * 사용할 Signature 해싱 알고리즘: HMAC-SHA256
+     * @param secret
+     */
+
     public JwtUtil(String secret) {
-        // 사용할 Signature 해싱 알고리즘: HMAC-SHA256
         this.key = Keys.hmacShaKeyFor(secret.getBytes());
     }
 
+    /**
+     * Claims: 페이로드에 담기는 데이터들
+     * Signature: 토큰이 위조되지 않았는지 검사
+     *
+     * @param userId
+     * @param name
+     * @return token
+     */
+
     public String createToken(long userId, String name) {
         String token = Jwts.builder()
-                .claim("userId", userId)  // Claims: 페이로드에 담기는 데이터들
+                .claim("userId", userId)
                 .claim("name", name)
-                .signWith(key, SignatureAlgorithm.HS256)  // Signature: 토큰이 위조되지 않았는지 검사
+                .signWith(key, SignatureAlgorithm.HS256)
                 .compact();
         
         return token;
+    }
+
+    /**
+     * 전달받은 토큰에 저장되어 있는 Claims 객체들을 반환
+     *
+     * @param token
+     * @return claims
+     */
+
+    public Claims getClaims(String token) {
+        Claims claims = Jwts.parserBuilder()
+                .setSigningKey(key)
+                .build()
+                .parseClaimsJws(token)
+                .getBody();
+
+        return claims;
     }
 }
