2af3621180a93d4e3de5d430fdbc5aeebaf32b6d
Alex <dnjscksdn98@gmail.com>
2020-06-24 10:08:03 +0900

    Claims 반환 함수 테스트

--- eatgo-common/src/test/java/com/alexcode/eatgo/utils/JwtUtilTest.java
+++ eatgo-common/src/test/java/com/alexcode/eatgo/utils/JwtUtilTest.java
@@ -1,19 +1,39 @@
 package com.alexcode.eatgo.utils;
 
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import static org.hamcrest.core.Is.is;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.core.StringContains.containsString;
 
 
 class JwtUtilTest {
 
+    private static String SECRET = "12345678901234567890123456789012";
+
+    private JwtUtil jwtUtil;
+
+    @BeforeEach
+    public void setUp() {
+        jwtUtil = new JwtUtil(SECRET);
+    }
+
     @Test
     public void createToken() {
-        String secret = "12345678901234567890123456789012";
-        JwtUtil jwtUtil = new JwtUtil(secret);
         String token = jwtUtil.createToken(1004L, "tester");
 
         assertThat(token, containsString("."));
+    }
+
+    @Test
+    public void getClaims() {
+        String token = jwtUtil.createToken(1004L, "tester");
+        Claims claims = jwtUtil.getClaims(token);
+
+        assertThat(claims.get("name"), is("tester"));
+        assertThat(claims.get("userId", Long.class), is(1004L));
     }
 }
