bd48be6be9eec98eb9ea94e5b7e0ef42b2fd02ca
Alex <dnjscksdn98@gmail.com>
2020-06-24 11:35:22 +0900

    수정된 리뷰 생성 테스트

--- eatgo-customer-api/src/test/java/com/alexcode/eatgo/application/ReviewServiceTest.java
+++ eatgo-customer-api/src/test/java/com/alexcode/eatgo/application/ReviewServiceTest.java
@@ -3,7 +3,6 @@ package com.alexcode.eatgo.application;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.verify;
 
-import com.alexcode.eatgo.domain.models.Review;
 import com.alexcode.eatgo.domain.ReviewRepository;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -26,13 +25,7 @@ class ReviewServiceTest {
 
   @Test
   public void addReview() {
-    Review review = Review.builder()
-        .name("alex")
-        .score(3)
-        .description("yummy")
-        .build();
-
-    reviewService.addReview(1004L, review);
+    reviewService.addReview(1004L, "tester", 3, "yummy");
 
     verify(reviewRepository).save(any());
   }
