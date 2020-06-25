11e2c04541ca0c2b41f3aa42dc43a54b324ec956
Alex <dnjscksdn98@gmail.com>
2020-06-25 12:01:42 +0900

    JWT 생성시 가게 주인이면 가게 ID도 페이로드에 포함

--- eatgo-common/src/main/java/com/alexcode/eatgo/utils/JwtUtil.java
+++ eatgo-common/src/main/java/com/alexcode/eatgo/utils/JwtUtil.java
@@ -1,6 +1,7 @@
 package com.alexcode.eatgo.utils;
 
 import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.JwtBuilder;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.security.Keys;
@@ -23,19 +24,25 @@ public class JwtUtil {
     /**
      * Claims: 페이로드에 담기는 데이터들
      * Signature: 토큰이 위조되지 않았는지 검사
+     * 
+     * 식당 주인이라면 식당 ID도 페이로드에 포함
      *
      * @param userId
      * @param name
+     * @param restaurantId
      * @return token
      */
 
-    public String createToken(long userId, String name) {
-        String token = Jwts.builder()
+    public String createToken(long userId, String name, Long restaurantId) {
+        JwtBuilder builder = Jwts.builder()
                 .claim("userId", userId)
-                .claim("name", name)
-                .signWith(key, SignatureAlgorithm.HS256)
-                .compact();
-        
+                .claim("name", name);
+
+        if(restaurantId != null) {
+            builder = builder.claim("restaurantId", restaurantId);
+        }
+
+        String token = builder.signWith(key, SignatureAlgorithm.HS256).compact();
         return token;
     }
 
