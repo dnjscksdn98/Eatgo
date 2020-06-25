57906295b89e54b36e8be470116fd7a815c95379
Alex <dnjscksdn98@gmail.com>
2020-06-25 12:04:07 +0900

    가게 주인 로그인시 JWT 생성 함수에 가게 ID를 파라미터로 전달

--- eatgo-login-api/src/main/java/com/alexcode/eatgo/interfaces/SessionController.java
+++ eatgo-login-api/src/main/java/com/alexcode/eatgo/interfaces/SessionController.java
@@ -32,7 +32,10 @@ public class SessionController {
     String password = resource.getPassword();
     User user = userService.authenticate(email, password);
 
-    String accessToken = jwtUtil.createToken(user.getId(), user.getName());
+    String accessToken = jwtUtil.createToken(
+            user.getId(),
+            user.getName(),
+            user.isRestaurantOwner() ? user.getRestaurantId() : null);
 
     SessionResponseDto sessionResponseDto =
         SessionResponseDto.builder().accessToken(accessToken).build();
