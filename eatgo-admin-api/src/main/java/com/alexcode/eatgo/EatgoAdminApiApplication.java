764258dd5e8fa979acfe6ca502ca3340edd2ec5e
Alex Won <dnjscksdn98@gmail.com>
2020-08-17 21:28:26 +0900

    feat: config eureka clients

--- eatgo-admin-api/src/main/java/com/alexcode/eatgo/EatgoAdminApiApplication.java
+++ eatgo-admin-api/src/main/java/com/alexcode/eatgo/EatgoAdminApiApplication.java
@@ -8,11 +8,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 import java.time.LocalDateTime;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class EatgoAdminApiApplication implements CommandLineRunner {
 
 	@Autowired
