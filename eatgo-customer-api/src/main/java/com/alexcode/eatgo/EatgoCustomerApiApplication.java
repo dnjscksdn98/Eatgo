362db58a75637fdf5888ff41b8c59a9ac8b13c3f
Alex Won <dnjscksdn98@gmail.com>
2020-08-17 21:28:26 +0900

    feat: config eureka clients

--- eatgo-customer-api/src/main/java/com/alexcode/eatgo/EatgoCustomerApiApplication.java
+++ eatgo-customer-api/src/main/java/com/alexcode/eatgo/EatgoCustomerApiApplication.java
@@ -2,8 +2,10 @@ package com.alexcode.eatgo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class EatgoCustomerApiApplication {
 
 	public static void main(String[] args) {
